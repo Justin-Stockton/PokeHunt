e778391afc76cac5c37bb650d475f10a4c56afc2
Justin-Stockton <justinstockton67@gmail.com>
2022-06-24 10:28:01 -0400

    skeleton for pokemon

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
+import pokeReducer from "./pokemon";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  pokeReducer,
 });
 
 let enhancer;
