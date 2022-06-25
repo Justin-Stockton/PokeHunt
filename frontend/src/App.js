69e479517a4c0b8584b58b873aa32a2869e6bae8
Justin-Stockton <justinstockton67@gmail.com>
2022-06-25 18:42:18 -0400

    pokemon component now displays the pokemons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,8 +28,8 @@ function App() {
           </Route>
         </Switch>
       )}
-      <Pokemon />
       <AddPokemon />
+      <Pokemon />
     </>
   );
 }
--- frontend/src/components/Pokemon/index.js
+++ frontend/src/components/Pokemon/index.js
@@ -2,28 +2,46 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // import { Redirect } from "react-router-dom";
 import { thunkGetAllPokemons } from "../../store/pokemon";
+import "./Pokemon.css";
+import { NavLink } from "react-router-dom";
 
 function Pokemon() {
   const dispatch = useDispatch();
-  const pokemon = useSelector((state) => {
-    return Object.values(state.pokemon).map(
-      (pokemonId) => state.pokemon[pokemonId]
-    );
+  const pokemonArr = useSelector((state) => {
+    return Object.values(state.pokemon);
   });
-  // console.log(pokemon);
-  //
+
   useEffect(() => {
     dispatch(thunkGetAllPokemons());
   }, [dispatch]);
-  //
-  if (!pokemon) {
+  if (!pokemonArr) {
     return null;
   }
   return (
     <>
-      <div>Hello From PokeComponet (products)</div>
-      <div>Container for pokemon</div>
-      <div className="primary-text">{pokemon.name}</div>
+      {pokemonArr.map((pokemon) => {
+        return (
+          <>
+            <div className="container">
+              <div className="poke-img">
+                <NavLink to={`/pokemon/${pokemon.id}`}>
+                  <img
+                    src={`${pokemon.imgUrl}`}
+                    style={{ height: "7rem", width: "7rem" }}
+                  />
+                </NavLink>
+              </div>
+
+              <div className="poke-text">
+                <div key={pokemon.name} className="poke-name">
+                  {pokemon.name}
+                </div>
+                <div className="poke-description">{pokemon.description}</div>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </>
   );
 }
