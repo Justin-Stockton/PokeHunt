dad818790e22568df96dfd74c6a8cb13a848857e
Justin-Stockton <justinstockton67@gmail.com>
2022-06-28 11:28:38 -0400

    added db url for production

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,7 +16,8 @@ module.exports = {
     seederStorage: "sequelize",
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable:
+      "postgres://jmpyfreypvtpdf:d99585ed1857e427ce16c885a0c448c295f956e73e686fd0eca0ca9736a83b22@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d6m1pgrf3qa7su",
     dialect: "postgres",
     seederStorage: "sequelize",
     dialectOptions: {
