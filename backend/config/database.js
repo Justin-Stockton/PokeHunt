c873d3b5e03a1f4ee4e5be7d81ee06ffe9dac11f
Justin-Stockton <justinstockton67@gmail.com>
2022-06-30 13:16:31 -0400

    test

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,8 +16,7 @@ module.exports = {
     seederStorage: "sequelize",
   },
   production: {
-    use_env_variable:
-      "postgres://jmpyfreypvtpdf:d99585ed1857e427ce16c885a0c448c295f956e73e686fd0eca0ca9736a83b22@ec2-44-198-82-71.compute-1.amazonaws.com:5432/d6m1pgrf3qa7su",
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     seederStorage: "sequelize",
     dialectOptions: {
