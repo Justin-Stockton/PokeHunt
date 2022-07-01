af2830d66ba9f4003acf4fd199cb33c3542b9de2
Justin-Stockton <justinstockton67@gmail.com>
2022-07-01 08:59:43 -0400

    added tech used

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -27,20 +27,100 @@ function Main() {
               <AddPokemon />
             </div>
             <div style={{ marginTop: "3%" }}>Created by:</div>
-            <div>
+            <div style={{ padding: "2%" }}>
               <a
                 href="https://www.github.com/Justin-Stockton"
                 target="https://www.github.com/Justin-Stockton"
               >
                 Justin Stockton
               </a>
+              <h3>Technologies used:</h3>
+              <div style={{ padding: "2%" }}>
+                <a
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    textDecoration: "none",
+                  }}
+                  href="https://reactjs.org/"
+                  target="https://reactjs.org/"
+                >
+                  <img
+                    style={{ height: "15%", width: "15%" }}
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+                  />
+                </a>
+              </div>
+              <div style={{ padding: "2%" }}>
+                <a
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    textDecoration: "none",
+                  }}
+                  href="https://redux.js.org/"
+                  target="https://redux.js.org/"
+                >
+                  <img
+                    style={{ height: "15%", width: "15%" }}
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
+                  />
+                </a>
+              </div>
+              <div style={{ padding: "2%" }}>
+                <a
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    textDecoration: "none",
+                  }}
+                  href="https://nodejs.org/en/"
+                  target="https://nodejs.org/en/"
+                >
+                  <img
+                    style={{ height: "15%", width: "15%" }}
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"
+                  />
+                </a>
+              </div>
+
+              <div style={{ padding: "2%" }}>
+                <a
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    textDecoration: "none",
+                  }}
+                  href="https://expressjs.com/"
+                  target="https://expressjs.com/"
+                >
+                  <img
+                    style={{ height: "15%", width: "15%" }}
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
+                  />
+                </a>
+              </div>
+
+              <div style={{ padding: "2%" }}>
+                <a
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    textDecoration: "none",
+                  }}
+                  href="https://www.postgresql.org/"
+                  target="https://www.postgresql.org/"
+                >
+                  <img
+                    style={{ height: "15%", width: "15%" }}
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg"
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div
-          className="Is it this"
-          style={{ width: "108rem", marginLeft: "10%", marginBottom: "2%" }}
-        >
+        <div style={{ width: "108rem", marginLeft: "10%", marginBottom: "2%" }}>
           <Pokemon />
         </div>
       </div>
